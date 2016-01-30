decc1df49bb4e94aa1ab8c72a501ce1864891c8c
bachtsui <dbtsui2@gmail.com>
2016-01-30 14:46:46 -0800

    Experimenting with getting values for hash

--- app/controllers/mutual_funds_controller.rb
+++ app/controllers/mutual_funds_controller.rb
@@ -9,9 +9,22 @@ class MutualFundsController < ApplicationController
     #@mutualfund = MutualFund.find_by_id(params[:id])
     @stocks = @mutualfund.stocks
 
-    # object = {}
+    # @industry_counts = {}
+    @industry_counts = @stocks.group(:industry).count
 
-    # object = @stocks.group(:industry).count
+    @h = { "a" => 100, "b" => 200 }
+    
+    # @temp = @industry_counts["Tobacco"]
+    # @temp2 = @industry_counts["Technology"]
+    # @temp = @industry_counts[:industry]
+
+    # @industry_counts.each_pair do |key,value|
+    	
+    # 	# temp2 = temp / @stocks.length
+    # 	# temp3 = temp2 * 100
+    # 	# temp3
+    # end
+
     # @array = []
 
     # @stocks.each do |x|
