8e3296b474a7c890590c595e2f468cb291a55e3d
bachtsui <dbtsui2@gmail.com>
2016-01-30 14:06:35 -0800

    Got industry to display in a hash

--- app/controllers/mutual_funds_controller.rb
+++ app/controllers/mutual_funds_controller.rb
@@ -5,11 +5,22 @@ class MutualFundsController < ApplicationController
   end
 
   def show
-    # @mutualfund = MutualFund.friendly.find(params[:id])
     @mutualfund = MutualFund.friendly.find(params[:id])
+    #@mutualfund = MutualFund.find_by_id(params[:id])
     @stocks = @mutualfund.stocks
 
+    # object = {}
+
+    # object = @stocks.group(:industry).count
+    # @array = []
+
+    # @stocks.each do |x|
+    # 	temp = x.industry
+    # 	@array << temp
+    # end 
+
     render :show
+
   end
 
 end
