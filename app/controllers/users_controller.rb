7f0810f887385f6cb0ac6ad7cc18bbac3cbd21a9
trebloc <ctse77@alum.berkeley.edu>
2016-01-30 16:52:07 -0800

    added edit and new pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
     @user = User.new
     render :new  
   end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    render :show
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
