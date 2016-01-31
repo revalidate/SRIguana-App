81abec0952d801e77cbf7f81857eeff6467efc2c
bachtsui <dbtsui2@gmail.com>
2016-01-31 15:29:40 -0800

    Added Delete for Users, full CRUD functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,13 +26,25 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find_by_id(params[:id])
+    @user = User.friendly.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user)
       flash[:notice] = "Successfully Updated Profile"
     else
       flash[:error] = @user.errors.full_messages.join(", ")
       redirect_to edit_user_path(@user)
+    end
+  end
+
+  def destroy
+    @user = User.friendly.find(params[:id])
+    if current_user == @user
+      @user.destroy
+      flash[:notice] = "Successfully deleted user #{@user.last_name}"
+      redirect_to root_path
+    else
+      flash[:notice] = "You can't delete that user."
+      redirect_to user_path(@user)
     end
   end
 
