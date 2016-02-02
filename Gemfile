85ba6ab8e5d167e0de20971e48faaf7a6738303a
revalidate <reva.bhatt@gmail.com>
2016-02-01 16:53:33 -0800

    added 3 gems pry-rails, better_errors, pry-bye-bug / added interactive add MF button that appends to user profile with validations

--- Gemfile
+++ Gemfile
@@ -46,6 +46,9 @@ gem "chartkick"
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry-rails'
+  gem 'pry-byebug'
+  gem 'better_errors'
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true,
+    format: { with: /[^@\s]+@(?:[-a-z0-9]+\.)+[a-z]{2,}/i,
+            message: 'format is invalid'}
+
+  validates :password, length: { in: 6...255}
+
   has_secure_password
   include FriendlyId
   friendly_id :last_name
@@ -10,10 +16,4 @@ class User < ActiveRecord::Base
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
   end  
-  
-  validates :email, presence: true, uniqueness: true,
-            format: { with: /[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{3}/i,
-            				message: 'format is invalid'}
-
-  validates :password, length: { in: 6...255}
 end
