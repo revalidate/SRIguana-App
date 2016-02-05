b7be02611f38230e9464e7134b93a5b4e488190f
nathanallen <thenathanator@gmail.com>
2016-02-05 15:51:15 -0800

    reorganize user class to follow convention

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,27 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :user_mutual_funds
+  has_many :mutual_funds, :through => :user_mutual_funds, dependent: :destroy
+
+  include FriendlyId
+  friendly_id :last_name,:use => [:slugged]
+
   validates :email, presence: true, uniqueness: true,
     format: { with: /[^@\s]+@(?:[-a-z0-9]+\.)+[a-z]{2,}/i,
             message: 'format is invalid'}
 
   validates :password, length: { in: 6...255},
              :on => :create
-        
+
   #Created a separate validation for edit from to allow
-  #for password field to be blank         
+  #for password field to be blank
   validates :password, length: { in: 6...255},
              :on => :update,
-             allow_blank: true     
-
-  has_secure_password
-  include FriendlyId
-  friendly_id :last_name,:use => [:slugged]
-
-  has_many :user_mutual_funds
-  has_many :mutual_funds, :through => :user_mutual_funds, dependent: :destroy
+             allow_blank: true
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
-  end  
+  end
 
 end
