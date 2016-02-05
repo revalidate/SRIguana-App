dcd2307eed5ac8cd3ab5dc86cae1aad59109d50b
nathanallen <thenathanator@gmail.com>
2016-02-05 15:31:58 -0800

    pass fewer objects into mf#index view -- use current_user helper, drill into @mutalfund

--- app/controllers/mutual_funds_controller.rb
+++ app/controllers/mutual_funds_controller.rb
@@ -12,17 +12,6 @@ class MutualFundsController < ApplicationController
   def show
     @title = "Portfolio"
     @mutualfund = MutualFund.friendly.find(params[:id])
-    @stocks = @mutualfund.stocks
-
-    @industry_counts = @mutualfund.stock_dollar_amounts_by_industry
-    #Inaccurate variable name for line 16, should be something like
-    #@investment_by_industry
-
-    if current_user !=nil
-      @user = User.friendly.find(current_user.id)
-    else
-      current_user = nil
-    end
   end
 
   def search
